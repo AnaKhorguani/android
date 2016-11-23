e066f0a75707e08ccb1076b75c10118bacdc7e91
AnaKhorguani <anakhorguani@yahoo.com>
2016-11-24 00:05:26 +0400

    run time request

--- SecondHomework/app/src/main/java/com/example/ana/secondhomework/MainActivity.java
+++ SecondHomework/app/src/main/java/com/example/ana/secondhomework/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.ana.secondhomework;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     private boolean list_visibile = false;
 
@@ -39,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkPermission())
+        {
 
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
@@ -109,8 +118,10 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                         else Toast.makeText(MainActivity.this, "You Clicked at file " + web.get(position), Toast.LENGTH_SHORT).show();
-                    }
+
+                }
                 });
+
 
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -147,6 +158,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        }
+        else {
+            requestPermission();
+        }
 
 
                 }
@@ -233,6 +248,38 @@ public void back()
             grid.setAdapter(gridadapter);
         }
 
+    }
+
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "READ External Storage permission allows us to do READ images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
     }
 
 }
